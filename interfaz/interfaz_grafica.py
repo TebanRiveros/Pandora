bae9a5badffb4e5521e0c10c99c56ecb32bc0282
DavidChoconta <drbeltranc@udistrital.edu.co>
2024-05-09 06:16:10 -0500

    Ya están las imagenes en las barras de menú tanto en la interfaz de diseño como de simulación, falta mirar los colores de los simbolos

--- interfaz/interfaz_grafica.py
+++ interfaz/interfaz_grafica.py
@@ -63,7 +63,7 @@ class VentanaSimulacion():
     def __init__(self):
         self.root = ctk.CTk()
         self.root.geometry("900x600")
-        self.root.title('Pandora S')
+        self.root.title('Pandora Simulación')
         wtotal = self.root.winfo_screenwidth()
         htotal = self.root.winfo_screenheight()
 
@@ -75,16 +75,51 @@ class VentanaSimulacion():
 
         self.root.geometry(str(wventana)+"x"+str(hventana)+"+"+str(pwidth)+"+"+str(pheight))
 
-        self.menu = ctk.CTkFrame(self.root, fg_color="blue")
+        self.menu = ctk.CTkFrame(self.root, fg_color="#003366")
         self.menu.pack(side=ctk.LEFT, fill='both', expand=False)
 
-        emulador=ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "imagen1.png")),dark_image=Image.open(os.path.join(carpeta_imagenes, "imagen1.png")), size=(150,60))
+        label = ctk.CTkLabel(self.menu, text="P A N D O R A", font=("Arial Black", 20))
+
+        label.place(relx=0.5, rely=0.1, anchor="center") 
+        
+        
+        sensor = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "sensor.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "sensor.png")), size=(100, 50))
+        # Display the image
+        image_sensor = ctk.CTkLabel(self.menu, text="", image=sensor)
+        image_sensor.place(x=2, y=120)
+        
+        acondicionador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "acondicionador.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "acondicionador.png")), size=(60, 90))
+        # Display the image
+        image_acondicionador = ctk.CTkLabel(self.menu, text="", image=acondicionador)
+        image_acondicionador.place(x=2, y=200)
+        
+        discretizador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "discretizador.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "discretizador.png")), size=(60, 90))
+        # Display the image
+        image_discretizador = ctk.CTkLabel(self.menu, text="", image=discretizador)
+        image_discretizador.place(x=2, y=320)
+        
+        emulador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "imagen1.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "imagen1.png")), size=(100, 50))
         # Display the image
         image_label = ctk.CTkLabel(self.menu, text="", image=emulador)
-        image_label.pack()
-
-
+        image_label.place(x=2, y=450)  # Ajusta las coordenadas (x, y) según sea necesario
+        
+        self.button_sensor = ctk.CTkButton(self.menu)
+        self.button_acondicionador = ctk.CTkButton(self.menu)
+        self.button_discretizador = ctk.CTkButton(self.menu)
+        self.button_emulador = ctk.CTkButton(self.menu)
+        
+        buttons_info = [
+            ("Sensor", "\FFCC70", self.button_sensor),
+            ("Acondicionador", "\FFCC70", self.button_acondicionador),
+            ("Discretizador", "\FFCC70", self.button_discretizador),
+            ("Emulador", "\FFCC70", self.button_emulador)
+        ]
+        #for text, icon, button in buttons_info: (ver video en el minuto 36:40) 
+           # self.configurar_boton_menu1(button, text, icon, )
+        
         self.root.mainloop()
+
+
 
 
 
@@ -92,16 +127,44 @@ class VentanaSimulacion():
 class VentanaDiseno():
     def __init__(self):
         self.root = ctk.CTk()
-        self.root.geometry("500x400")
-        self.root.title('Pandora D')
+        self.root.geometry("900x600")
+        self.root.title('Pandora Diseño')
         wtotal = self.root.winfo_screenwidth()
         htotal = self.root.winfo_screenheight()
 
-        wventana = 400
-        hventana = 300
+        wventana = 900
+        hventana = 600
 
         pwidth = round(wtotal/2-wventana/2)
         pheight = round(htotal/2-hventana/2)
 
         self.root.geometry(str(wventana)+"x"+str(hventana)+"+"+str(pwidth)+"+"+str(pheight))
+        
+        self.menu = ctk.CTkFrame(self.root, fg_color="#003366")
+        self.menu.pack(side=ctk.LEFT, fill='both', expand=False)
+
+        label = ctk.CTkLabel(self.menu, text="P A N D O R A", font=("Arial Black", 20))
+
+        label.place(relx=0.5, rely=0.1, anchor="center") 
+        
+        
+        sensor = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "sensor.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "sensor.png")), size=(100, 50))
+        # Display the image
+        image_sensor = ctk.CTkLabel(self.menu, text="", image=sensor)
+        image_sensor.place(x=2, y=120)
+        
+        acondicionador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "acondicionador.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "acondicionador.png")), size=(60, 90))
+        # Display the image
+        image_acondicionador = ctk.CTkLabel(self.menu, text="", image=acondicionador)
+        image_acondicionador.place(x=2, y=200)
+        
+        discretizador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "discretizador.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "discretizador.png")), size=(60, 90))
+        # Display the image
+        image_discretizador = ctk.CTkLabel(self.menu, text="", image=discretizador)
+        image_discretizador.place(x=2, y=320)
+        
+        emulador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "imagen1.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "imagen1.png")), size=(100, 50))
+        # Display the image
+        image_label = ctk.CTkLabel(self.menu, text="", image=emulador)
+        image_label.place(x=2, y=450)  # Ajusta las coordenadas (x, y) según sea necesario
         self.root.mainloop()
