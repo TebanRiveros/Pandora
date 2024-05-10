4f6fc38f244662ad26e273c23eb38bcf43a6b325
DavidChoconta <drbeltranc@udistrital.edu.co>
2024-05-10 10:50:53 -0500

    Se cambiaron los simbolos, se agregó los botones diseño y simular

--- interfaz/interfaz_grafica.py
+++ interfaz/interfaz_grafica.py
@@ -83,47 +83,33 @@ class VentanaSimulacion():
         #label.place(relx=0.5, rely=0.1, anchor="center") 
         self.label.pack(side=ctk.TOP)
         
+        self.instrumento1 = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "instrumento1.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "instrumento1.png")), size=(100, 50))
         
-        self.isensor = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "sensor2.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "sensor2.png")), size=(100, 50))
-        # # Display the image
-        # image_sensor = ctk.CTkLabel(self.menu, text="", image=sensor)
-        # image_sensor.place(x=2, y=120)
-        
+        self.isensor = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "sensor.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "sensor.png")), size=(90, 50))
+    
         self.iacondicionador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "acondicionador.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "acondicionador.png")), size=(60, 90))
-        # # Display the image
-        # image_acondicionador = ctk.CTkLabel(self.menu, text="", image=acondicionador)
-        # image_acondicionador.place(x=2, y=200)
-        
+       
         self.idiscretizador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "discretizador.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "discretizador.png")), size=(60, 90))
-        # # Display the image
-        # image_discretizador = ctk.CTkLabel(self.menu, text="", image=discretizador)
-        # image_discretizador.place(x=2, y=320)
-        
-        self.iemulador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "imagen1.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "imagen1.png")), size=(100, 50))
-        # # Display the image
-        # image_label = ctk.CTkLabel(self.menu, text="", image=emulador)
-        # image_label.place(x=2, y=450)  # Ajusta las coordenadas (x, y) según sea necesario
-        
+     
+        self.iemulador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "imagen1.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "imagen1.png")), size=(90, 50))
+        self.play = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "play.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "play.png")), size=(37, 30))
+
+        self.button_instrumento1 = ctk.CTkButton(self.menu, text="Instrumento", image=self.instrumento1, width=220)
         self.button_sensor = ctk.CTkButton(self.menu, text="Sensor", image=self.isensor, width=220)
         self.button_acondicionador = ctk.CTkButton(self.menu, text="Acondicionador", image=self.iacondicionador, width=220)
         self.button_discretizador = ctk.CTkButton(self.menu, text="Discretizador", image=self.idiscretizador, width=220)
         self.button_emulador = ctk.CTkButton(self.menu, text="Emulador", image=self.iemulador, width=220)
+       
+        self.button_simular = ctk.CTkButton(self.menu, text="Simular",image=self.play, font=("Arial", 15), corner_radius=32, border_width=1.5)
+        
+        self.button_simular.place(relx=0.5, rely=0.8, anchor=tkinter.CENTER)
 
+        self.button_instrumento1.pack(side=ctk.TOP)
         self.button_sensor.pack(side=ctk.TOP)
         self.button_acondicionador.pack(side=ctk.TOP)
         self.button_discretizador.pack(side=ctk.TOP)
         self.button_emulador.pack(side=ctk.TOP)
 
-        
-
-        # buttons_info = [
-        #     ("Sensor", "\FFCC70", self.button_sensor),
-        #     ("Acondicionador", "\FFCC70", self.button_acondicionador),
-        #     ("Discretizador", "\FFCC70", self.button_discretizador),
-        #     ("Emulador", "\FFCC70", self.button_emulador)
-        # ]
-        #for text, icon, button in buttons_info: (ver video en el minuto 36:40) 
-           # self.configurar_boton_menu1(button, text, icon, )
         
         self.root.mainloop()
 
@@ -148,31 +134,38 @@ class VentanaDiseno():
 
         self.root.geometry(str(wventana)+"x"+str(hventana)+"+"+str(pwidth)+"+"+str(pheight))
         
-        self.menu = ctk.CTkFrame(self.root, fg_color="#003366")
+        self.menu = ctk.CTkFrame(self.root, fg_color="#1F6AA5",width=220)
         self.menu.pack(side=ctk.LEFT, fill='both', expand=False)
 
-        label = ctk.CTkLabel(self.menu, text="P A N D O R A", font=("Arial Black", 20))
+        self.label = ctk.CTkLabel(self.menu, text="\nP A N D O R A\n", font=("Arial Black", 20), padx=30)
 
-        label.place(relx=0.5, rely=0.1, anchor="center") 
+        #label.place(relx=0.5, rely=0.1, anchor="center") 
+        self.label.pack(side=ctk.TOP)
         
+        self.instrumento1 = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "instrumento1.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "instrumento1.png")), size=(100, 50))
         
-        sensor = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "sensor.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "sensor.png")), size=(100, 50))
-        # Display the image
-        image_sensor = ctk.CTkLabel(self.menu, text="", image=sensor)
-        image_sensor.place(x=2, y=120)
+        self.isensor = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "sensor.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "sensor.png")), size=(90, 50))
+    
+        self.iacondicionador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "acondicionador.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "acondicionador.png")), size=(60, 90))
+       
+        self.idiscretizador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "discretizador.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "discretizador.png")), size=(60, 90))
+     
+        self.iemulador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "imagen1.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "imagen1.png")), size=(90, 50))
+        self.disenar = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "disenar.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "disenar.png")), size=(40, 30))
+      
+        self.button_instrumento1 = ctk.CTkButton(self.menu, text="Instrumento", image=self.instrumento1, width=220)
+        self.button_sensor = ctk.CTkButton(self.menu, text="Sensor", image=self.isensor, width=220)
+        self.button_acondicionador = ctk.CTkButton(self.menu, text="Acondicionador", image=self.iacondicionador, width=220)
+        self.button_discretizador = ctk.CTkButton(self.menu, text="Discretizador", image=self.idiscretizador, width=220)
+        self.button_emulador = ctk.CTkButton(self.menu, text="Emulador", image=self.iemulador, width=220)
+       
+        self.button_disenar = ctk.CTkButton(self.menu, text="Diseñar",image=self.disenar, font=("Arial", 15), corner_radius=32, border_width=1.5)
         
-        acondicionador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "acondicionador.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "acondicionador.png")), size=(60, 90))
-        # Display the image
-        image_acondicionador = ctk.CTkLabel(self.menu, text="", image=acondicionador)
-        image_acondicionador.place(x=2, y=200)
-        
-        discretizador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "discretizador.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "discretizador.png")), size=(60, 90))
-        # Display the image
-        image_discretizador = ctk.CTkLabel(self.menu, text="", image=discretizador)
-        image_discretizador.place(x=2, y=320)
-        
-        emulador = ctk.CTkImage(light_image=Image.open(os.path.join(carpeta_imagenes, "imagen1.png")), dark_image=Image.open(os.path.join(carpeta_imagenes, "imagen1.png")), size=(100, 50))
-        # Display the image
-        image_label = ctk.CTkLabel(self.menu, text="", image=emulador)
-        image_label.place(x=2, y=450)  # Ajusta las coordenadas (x, y) según sea necesario
+        self.button_disenar.place(relx=0.5, rely=0.8, anchor=tkinter.CENTER)
+
+        self.button_instrumento1.pack(side=ctk.TOP)
+        self.button_sensor.pack(side=ctk.TOP)
+        self.button_acondicionador.pack(side=ctk.TOP)
+        self.button_discretizador.pack(side=ctk.TOP)
+        self.button_emulador.pack(side=ctk.TOP)
         self.root.mainloop()
